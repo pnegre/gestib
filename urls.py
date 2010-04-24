d3bcefb3355cc419c39d29b2515533513453dabf
pnegre <pnegre@yorkie.(none)>
2010-04-24 09:39:27 +0200

    Anem fent...

--- urls.py
+++ urls.py
@@ -5,5 +5,6 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
 	
 	(r'^import/$', 'gestib.views.importData'),
+	(r'^doimport/$', 'gestib.views.doImport'),
 	
 )
--- views.py
+++ views.py
@@ -9,70 +9,82 @@ from xml.dom.minidom import parse, parseString
 from gestib.models import *
 
 
+def importProfessors(dom):
+	professors = dom.getElementsByTagName('PROFESSOR')
+	for prof in professors:
+		p = Professor(
+			codi = prof.getAttribute('codi'),
+			nom = prof.getAttribute('nom'),
+			llinatge1 = prof.getAttribute('ap1'),
+			llinatge2 = prof.getAttribute('ap2'),
+		)
+		p.save()
+
+
+def importCursos(dom):
+	cursos = dom.getElementsByTagName('CURS')
+	for curs in cursos:
+		c = Curs(nom=curs.getAttribute('descripcio'),codi=curs.getAttribute('codi'))
+		c.save()
+		
+		grups = curs.getElementsByTagName('GRUP')
+		for grup in grups:
+			prof = Professor.objects.filter(codi=grup.getAttribute('tutor'))
+			if prof == None: continue
+			if len(prof) == 0: continue
+			
+			g = Grup(
+				nom = grup.getAttribute('nom'),
+				curs = c,
+				tutor = prof[0],
+				codi = grup.getAttribute('codi'),
+			)
+			g.save()
+
+def importAlumnes(dom):
+	alumnes = dom.getElementsByTagName('ALUMNE')
+	for alumne in alumnes:
+		nom = alumne.getAttribute('nom')
+		l1 = alumne.getAttribute('ap1')
+		l2 = alumne.getAttribute('ap2')
+		exp = alumne.getAttribute('expedient')
+		gp = Grup.objects.filter(codi=alumne.getAttribute('grup'))
+		if gp == None: continue
+		if len(gp) == 0: continue
+		
+		a = Alumne(nom=nom,llinatge1=l1,llinatge2=l2,expedient=exp,grup=gp[0])
+		a.save()
+
+def importSubmateries(dom):
+	submateries = dom.getElementsByTagName('SUBMATERIES')[0].getElementsByTagName('SUBMATERIA')
+	for submateria in submateries:
+		codi = submateria.getAttribute('codi')
+		descripcio = submateria.getAttribute('descripcio')
+		nom = submateria.getAttribute('curta')
+		curs = Curs.objects.filter(codi=submateria.getAttribute('curs'))
+		
+		sbm = Submateria(nom=nom,descripcio=descripcio,codi=codi,curs=curs[0])
+		sbm.save()
+
+
+
 def importData(request):
-	ok = False
+	return render_to_response(
+			'gestib/import.html', {
+	} )
+
+
+
+def doImport(request):
 	if request.method == 'POST':
 		f = request.FILES['file']
 		dom = parse(f)
 		
-		professors = dom.getElementsByTagName('PROFESSOR')
-		for prof in professors:
-			p = Professor(
-				codi = prof.getAttribute('codi'),
-				nom = prof.getAttribute('nom'),
-				llinatge1 = prof.getAttribute('ap1'),
-				llinatge2 = prof.getAttribute('ap2'),
-			)
-			p.save()
-		
-		cursos = dom.getElementsByTagName('CURS')
-		for curs in cursos:
-			c = Curs(nom=curs.getAttribute('descripcio'),codi=curs.getAttribute('codi'))
-			c.save()
-			
-			grups = curs.getElementsByTagName('GRUP')
-			for grup in grups:
-				prof = Professor.objects.filter(codi=grup.getAttribute('tutor'))
-				if prof == None: continue
-				if len(prof) == 0: continue
-				
-				g = Grup(
-					nom = grup.getAttribute('nom'),
-					curs = c,
-					tutor = prof[0],
-					codi = grup.getAttribute('codi'),
-				)
-				g.save()
-		
-		
-		alumnes = dom.getElementsByTagName('ALUMNE')
-		for alumne in alumnes:
-			nom = alumne.getAttribute('nom')
-			l1 = alumne.getAttribute('ap1')
-			l2 = alumne.getAttribute('ap2')
-			exp = alumne.getAttribute('expedient')
-			gp = Grup.objects.filter(codi=alumne.getAttribute('grup'))
-			if gp == None: continue
-			if len(gp) == 0: continue
-			
-			a = Alumne(nom=nom,llinatge1=l1,llinatge2=l2,expedient=exp,grup=gp[0])
-			a.save()
-		
-		submateries = dom.getElementsByTagName('SUBMATERIES')[0].getElementsByTagName('SUBMATERIA')
-		for submateria in submateries:
-			codi = submateria.getAttribute('codi')
-			descripcio = submateria.getAttribute('descripcio')
-			nom = submateria.getAttribute('curta')
-			curs = Curs.objects.filter(codi=submateria.getAttribute('curs'))
-			
-			sbm = Submateria(nom=nom,descripcio=descripcio,codi=codi,curs=curs[0])
-			sbm.save()
-		
-		ok = True
+		importProfessors(dom)
+		importCursos(dom)
+		importAlumnes(dom)
+		importSubmateries(dom)
 	
 	return render_to_response(
-			'gestib/import.html', {
-				'ok': ok,
+			'gestib/ok.html', {
 	} )
-
-
