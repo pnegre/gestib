6e91bbc73e9e2e18ad491889e70a45c5cdd5c78f
pnegre <pere.negre@gmail.com>
2014-09-16 23:43:34 +0200

    Anem fent

---
+++ forms.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from django import forms
+from gestib.models import *
+
+
+class ImportForm(forms.Form):
+    anny = forms.ChoiceField(label="Any acadèmic")
+    fle = forms.FileField(label="Fitxer amb les dades XML (gestib)")
+
+    def __init__(self, *args, **kwrds):
+        super(ImportForm, self).__init__(*args, **kwrds)
+        self.fields['anny'].choices = [[ x.id, x] for x in Any.objects.all() ]
