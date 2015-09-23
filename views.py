c2d2766d6b027becae5c8f25c43b8f9e63cf60e2
pnegre <pere.negre@gmail.com>
2015-09-24 00:12:39 +0200

    Anem fent

--- views.py
+++ views.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import random, string
+
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.http import HttpResponse
 from django.utils import simplejson
@@ -62,3 +64,55 @@ def importData(request):
     return renderResponse(request, 'gestib/import.html', {
         'form': form,
     } )
+
+def stripchars(s):
+    s = s.replace(u"á","a")
+    s = s.replace(u"é","e")
+    s = s.replace(u"í","i")
+    s = s.replace(u"ó","o")
+    s = s.replace(u"ú","u")
+    s = s.replace(u"à","a")
+    s = s.replace(u"è","e")
+    s = s.replace(u"ì","i")
+    s = s.replace(u"ò","o")
+    s = s.replace(u"ù","u")
+    s = s.replace(u"ñ","n")
+    s = s.replace(u"ç","c")
+    s = s.replace(" ", "")
+    return s
+
+def tractaAlumne(al):
+    pwd = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(8))
+    l1 = stripchars(al.llinatge1.lower())
+    l2 = stripchars(al.llinatge2.lower())
+    nm = stripchars(al.nom.lower())
+    username = nm[0] + l1 + l2[0]
+    return "null@callisto.esliceu.com;Palma;%s;%s;%s;%s;%s\n" % (al.llinatge1, al.llinatge2, al.nom, username, pwd)
+
+# Per consultar alumnes d'un grup
+@permission_required('gestib.importar_alumnes')
+def consultaGrup(request):
+    any_actual = Any.objects.latest('any1')
+
+    if request.method == 'POST':
+        post = request.POST
+        gid = post['grup']
+        grup = Grup.objects.get(id=gid)
+        result = ""
+        result += "email;city;lastname1;lastname2;firstname;username;password_clear\n"
+        for m in Matricula.objects.filter(anny=any_actual, grup=grup):
+            result += tractaAlumne(m.alumne)
+
+        print result
+
+    else:
+        cursos = Curs.objects.filter(anny=any_actual)
+        grups = []
+        for c in cursos:
+            for g in Grup.objects.filter(curs=c):
+                grups.append(g)
+
+        return renderResponse(request, 'gestib/consulta.html', {
+            'grups': grups,
+            'anny': any_actual,
+        })
