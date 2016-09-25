3562cf1dde6a23e98d8d02f288e92f2b6ac46d41
pnegre <pere.negre@gmail.com>
2016-09-25 10:33:22 +0200

    Anem fent

--- forms.py
+++ forms.py
@@ -7,6 +7,10 @@ from gestib.models import *
 class ImportForm(forms.Form):
     anny = forms.ChoiceField(label="Any acadèmic")
     fle = forms.FileField(label="Fitxer amb les dades XML (gestib)")
+    importAlumnes = forms.BooleanField(label="Importar alumnes", initial=False, required=False)
+    importProfes = forms.BooleanField(label="Importar profes", initial=False, required=False)
+    importCursos = forms.BooleanField(label="Importar cursos", initial=False, required=False)
+    importSubmateries = forms.BooleanField(label="Importar submatèries", initial=False, required=False)
 
     def __init__(self, *args, **kwrds):
         super(ImportForm, self).__init__(*args, **kwrds)
--- views.py
+++ views.py
@@ -19,28 +19,28 @@ from gestib.forms import *
 def renderResponse(request,tmpl,dic):
     return render_to_response(tmpl, dic, context_instance=RequestContext(request))
 
+#
+# def cleanBeforeImport(anny):
+#     # Fem actiu només l'any actual
+#     Any.objects.all().update(actiu=False)
+#     anny.active = True
+#     anny.save()
+#
+#     # Esborrem totes les matrícules d'aquest any
+#
+#     # Desactivem tots els alumnes
+#
+#     # Desactivem tots els profes
+#
+#     # Desactivem tots els cursos
+#
+#     # Desactivem tots els grups
 
-def cleanBeforeImport(anny):
-    # Fem actiu només l'any actual
-    Any.objects.all().update(actiu=False)
-    anny.active = True
-    anny.save()
+def getOption(dct, attr):
+    if attr not in dct.keys():
+        return False
 
-    # Esborrem totes les matrícules d'aquest any
-    mats = Matricula.objects.filter(anny=anny).delete()
-
-    # Desactivem tots els alumnes
-    Alumne.objects.all().update(actiu=False)
-
-    # Desactivem tots els profes
-    Professor.objects.all().update(actiu=False)
-
-    # Desactivem tots els cursos
-    Curs.objects.all().update(actiu=False)
-
-    # Desactivem tots els grups
-    Grup.objects.all().update(actiu=False)
-
+    return dct[attr]
 
 
 # Mostra el form per importar dades de l'xml del gestib
@@ -52,6 +52,10 @@ def importData(request):
             annyid = form.cleaned_data['anny']
             fle = form.cleaned_data['fle']
             anny = Any.objects.get(id=annyid)
+            ials = getOption(form.cleaned_data, 'importAlumnes')
+            iprofs = getOption(form.cleaned_data, 'importProfessors')
+            icursos = getOption(form.cleaned_data, 'importCursos')
+            isubmats = getOption(form.cleaned_data, 'importSubmateries')
 
             dom = parse(fle)
             if anny.any1 != getYear(dom):
@@ -62,16 +66,33 @@ def importData(request):
                     'message': message
                 })
 
-            # Netejam abans d'importar
-            cleanBeforeImport(anny)
-
             # Importem les dades!!!
             incidencies = []
-            nprofs = importProfessors(incidencies, dom)
-            ncursos, ngrups = importCursos(incidencies, dom, anny)
-            nalumnes, nmats = importAlumnes(incidencies, dom, anny)
-            nsubs = importSubmateries(dom, anny)
-            nsubmatgrup = importSubmateriesGrup(dom)
+            nprofs, ncursos, ngrups, nalumnes, nmats, nsubs, nsubmatgrup = 0,0,0,0,0,0,0
+
+            if iprofs:
+                Professor.objects.all().update(actiu=False)
+                nprofs = importProfessors(incidencies, dom)
+
+            if icursos:
+                Curs.objects.all().update(actiu=False)
+                Grup.objects.all().update(actiu=False)
+                ncursos, ngrups = importCursos(incidencies, dom, anny)
+
+            if ials:
+                mats = Matricula.objects.filter(anny=anny).delete()
+                Alumne.objects.all().update(actiu=False)
+                nalumnes, nmats = importAlumnes(incidencies, dom, anny)
+
+            if isubmats:
+                # Alerta: no desactivem les submatèries. L'atribut "actiu" aquí ho emprem per altres coses...
+                nsubs = importSubmateries(dom, anny)
+                nsubmatgrup = importSubmateriesGrup(dom)
+
+            # Fem actiu només l'any actual
+            Any.objects.all().update(actiu=False)
+            anny.active = True
+            anny.save()
 
             return renderResponse(request, 'gestib/import.html', {
                 'finished': True,
